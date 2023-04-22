390982605c9b9115aba8952e60a29b4fdba8e781
Rebeca <rebecaalves145@gmail.com>
2023-04-22 17:40:26 -0300

    ajustes realizados na assistente virtual

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import json
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from unidecode import unidecode
+from bs4 import BeautifulSoup
 import wikipedia
+import pyttsx3
 
 with open('config.json', mode='r', encoding='utf-8') as arquivoConfiguracoes:
     config = json.load(arquivoConfiguracoes)
@@ -39,7 +41,7 @@ def verificarPergunta(texto):
     textoFormatado = " ".join(textoFormatado)
     for pergunta in formatarPerguntasConfig(perguntas):
         if pergunta == textoFormatado:
-            print("Achei a pergunta")
+            print("Aguarde...")
             buscarConteudo(textoSemPrefixoAssistente)
             break
 
@@ -47,19 +49,25 @@ def verificarPergunta(texto):
 def buscarConteudo(texto):
     wikipedia.set_lang("pt")
     conteudo = wikipedia.page(texto)
-    print(conteudo.content)
+    soup = BeautifulSoup(conteudo.html(), 'html.parser')
+    respostaEsperada = soup.find('p').text
+    print(respostaEsperada)
+    engine = pyttsx3.init()
+    engine.say(respostaEsperada)
+    engine.runAndWait()
 
 
+print("Olá, eu sou a " + prefixoAssistente +
+      " e estou aqui para te ajudar.")
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Diga alguma coisa: ")
+    print("Qual a sua dúvida sobre o renascimento? ")
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
     try:
         texto = r.recognize_google(audio, language='pt-BR')
         if texto.lower().startswith(prefixoAssistente.lower()):
-            print(texto)
+            print("Sua pergunta: " + texto)
             verificarPergunta(texto)
-
     except:
-        print("Não entendi")
+        print("Desculpa, não encontrei uma resposta!")
